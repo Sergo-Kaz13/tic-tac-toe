fff99857515d40441fec48b4f0fe87ff476e9f6a
Sergo_Kaz13 <fenrill1366613@gmail.com>
2023-01-29 11:59:00 +0200

    add logic to the game, determine the winner, show the try again button, reset the value of the game on the field, add a winning point to the winner, make a player name input window...

--- main.js
+++ main.js
@@ -1,24 +1,150 @@
 let nextPlayer = true;
 let spoilerText = "x";
+const boardText = [];
 
-const board = document.querySelector(".board");
+const userOneCount = document.querySelector(".user-one-count");
+const userTwoCount = document.querySelector(".user-two-count");
 const spoiler = document.querySelector(".spoiler");
+const board = document.querySelector(".board");
 
 board.addEventListener("click", handleClick);
 
 spoiler.textContent = spoilerText;
 
 function handleClick(e) {
-    if (e.target.localName === "td") {
-        console.log(true);
+    let item = e.target;
+    if (item.localName === "td" && !item.textContent) {
         if (nextPlayer) {
-            e.target.classList.add("tic");
+            item.innerText = "X";
         } else {
-            e.target.classList.add("tac");
+            item.innerText = "O";
+        }
+
+        nextPlayer = !nextPlayer;
+        spoilerText = nextPlayer ? "x" : "o";
+        spoiler.textContent = spoilerText;
+
+        getItemsBoard();
+    }
+}
+
+function getItemsBoard() {
+    const boardElement = document.getElementsByTagName("td");
+
+    boardText.length = 0;
+
+    for (const iterator of boardElement) {
+        boardText.push(iterator.innerText);
+    }
+
+    getDataBoard();
+}
+
+function getDataBoard() {
+    let row1 = "",
+        row2 = "",
+        row3 = "",
+        col1 = "",
+        col2 = "",
+        col3 = "",
+        diagonal1 = "",
+        diagonal2 = "";
+
+    for (let i = 0; i < boardText.length; i++) {
+        if (i < 3) {
+            row1 += boardText[i];
+        } else if (i < 6) {
+            row2 += boardText[i];
+        } else if (i < boardText.length) {
+            row3 += boardText[i];
+        }
+
+        if (i === 0 || i === 3 || i === 6) {
+            col1 += boardText[i];
+        }
+
+        if (i === 1 || i === 4 || i === 7) {
+            col2 += boardText[i];
+        }
+
+        if (i === 2 || i === 5 || i === 8) {
+            col3 += boardText[i];
+        }
+
+        if (i === 0 || i === 4 || i === 8) {
+            diagonal1 += boardText[i];
+        }
+
+        if (i === 2 || i === 4 || i === 6) {
+            diagonal2 += boardText[i];
         }
     }
 
-    nextPlayer = !nextPlayer;
-    spoilerText = nextPlayer ? "x" : "o";
-    spoiler.textContent = spoilerText;
+    const structuredDataBoard = [
+        row1,
+        row2,
+        row3,
+        col1,
+        col2,
+        col3,
+        diagonal1,
+        diagonal2,
+    ];
+
+    determineTheWinner(structuredDataBoard);
+}
+
+function determineTheWinner(boardData) {
+    boardData.forEach((item) => {
+        if (item === "XXX") {
+            showWinner(document.querySelector(".user-one").value);
+            showUserCount("x");
+        } else if (item === "OOO") {
+            showWinner(document.querySelector(".user-two").value);
+            showUserCount("o");
+        }
+    });
+}
+
+function showWinner(winner) {
+    console.log(["winner"], winner);
+
+    const winnerByLetters = winner.split("");
+
+    const wrapper = document.querySelector(".wrapper");
+    const canvasWinner = document.createElement("div");
+    const titleWinner = document.createElement("h1");
+    const btnRestart = document.createElement("button");
+
+    canvasWinner.classList = "canvas-winner";
+    titleWinner.classList = "title-winner";
+    btnRestart.classList = "btn-restart";
+
+    btnRestart.addEventListener("click", restartGame);
+
+    titleWinner.innerHTML = `winner: `;
+    btnRestart.innerText = "try again";
+
+    winnerByLetters.forEach((letter) => {
+        titleWinner.innerHTML += `<span>${letter}</span>`;
+    });
+
+    canvasWinner.append(titleWinner);
+    canvasWinner.append(btnRestart);
+    wrapper.append(canvasWinner);
+}
+
+function restartGame() {
+    for (const item of document.querySelectorAll("td")) {
+        item.innerHTML = "";
+    }
+    document.querySelector(".canvas-winner").remove();
+}
+
+function showUserCount(user) {
+    if (user === "x") {
+        userOneCount.textContent++;
+    } else if (user === "o") {
+        userTwoCount.textContent++;
+    }
 }
